291ef3e66df5baf54f3f5cba63f0cbd1b7e0aee5
Evan Moran <moraneva@msu.edu>
2016-09-21 19:45:06 -0400

    finishing up basic event converter + tests

--- modules/facebookEventConverter.js
+++ modules/facebookEventConverter.js
@@ -3,16 +3,57 @@
  */
 var facebookEventConverter = {
 
-    convertEvent: function (event) {
-        return {
-            UID: event.entry[0].messaging[0].sender.id,
-            message: {
-                payload: event.entry[0].messaging[0].message.text,
-                action: "text"
-            }
-        }
-    }
+  /**
+   *  This function converts the incoming facebook event into a generalized event that can be
+   *  used by the system. Currently this is returning the following object:
+   *  {
+   *    UID: unique user Id
+   *    message:{
+   *      payload: payload of message (could just be text).
+   *      action: action of message(text, postback, etc.).
+   *    }
+   *  } 
+   *
+   */
+  convertEvent: function (event) {
 
-};
+    var messageObjects = [];
+
+    // Entry is an array of enrties so we need to make sure to hit all of them.
+    event.entry.forEach(function(entry){
+
+      //Messaging is an array of messaging objects so we need to make sure to hit all of them.
+      entry.messaging.forEach(function(messaging){
+
+        //If message is part of this object, we are processing simple text message
+        if(messaging.message){
+          messageObjects.push({
+            UID: messaging.sender.id,
+            message: {
+              payload: messaging.message.text,
+              action: "text"
+            }
+          });
+        }
+
+        //If postback is part of this object, then we are processing postback message
+        else if(messaging.postback){
+          messageObjects.push({
+            UID: messaging.sender.id,
+            message: {
+              payload: messaging.postback.payload,
+              action: "postback"
+            }
+          }); 
+        }
+
+
+      });
+
+    });
+
+    return messageObjects;
+  }
+}
 
 module.exports = facebookEventConverter;
--- tests/test.js
+++ tests/test.js
@@ -3,52 +3,86 @@
  */
 var facebookEventConverter = require('../modules/facebookEventConverter');
 var expect = require('chai').expect;
+
 //This is a basic FB event
-var facebookEvent = {
+describe('Facebook Event Converter',function(){
+
+  describe('convertEvent',function(){
+    it('returns correct object for given facebook message event',function(){
+
+
+      //Simple text message.
+      var facebookEvent = {
         "object": "page",
         "entry": [
         {
-                "id": "671648416325901",
-                "time": 1474479140224,
-                "messaging": [
-                {
-                        "sender": {
-                                "id": "1114852861896648"
-                        },
-                        "recipient": {
-                                "id": "671648416325901"
-                        },
-                        "timestamp": 1474479140179,
-                        "message": {
-                                "mid": "mid.1474479140172:fddaed0caa2f0e4589",
-                                "seq": 7,
-                                "text": "hey"
-                        }
-                }
-                ]
+          "id": "671648416325901",
+          "time": 1474479140224,
+          "messaging": [
+          {
+            "sender": {
+              "id": "1114852861896648"
+            },
+            "recipient": {
+              "id": "671648416325901"
+            },
+            "timestamp": 1474479140179,
+            "message": {
+              "mid": "mid.1474479140172:fddaed0caa2f0e4589",
+              "seq": 7,
+              "text": "hey"
+            }
+          }]
+        }]
+      };
+      var event = facebookEventConverter.convertEvent(facebookEvent);
+
+      expect(event[0]).to.deep.equal({
+        UID: "1114852861896648",
+        message: {
+          payload: "hey",
+          action: "text"
+        }
+      });
+    });
+
+    it('returns correct object for given facebook postback event', function(){
+
+      var facebookEvent = {
+        "object": "page",
+        "entry": [
+        {
+          "id": "671648416325901",
+          "time": 1474479140224,
+          "messaging": [
+          {
+            "sender": {
+              "id": "1114852861896648"
+            },
+            "recipient": {
+              "id": "671648416325901"
+            },
+            "timestamp": 1474479140179,
+            "postback":{
+              "payload": "ITEM_LOOKUP" 
+            }
+          }
+          ]
         }
         ]
-};
+      };
 
-describe('Facebook Event Converter',function(){
+    var event = facebookEventConverter.convertEvent(facebookEvent);
 
-        describe('convertEvent',function(){
-                it('returns correct object for given facebook event',function(){
-                        var event = facebookEventConverter.convertEvent(facebookEvent);
-                        console.log(JSON.stringify(event)); 
-                        console.log(JSON.stringify({UID: "1114852861896648",
-                                message: {
-                                        payload: "hey",
-                                        action: "text"
-                                }}));
-                        expect(event).to.equal({
-                                UID: "1114852861896648",
-                                message: {
-                                        payload: "hey",
-                                        action: "text"
-                                }
-                        });
-                });
-        });
+    expect(event[0]).to.deep.equal({
+      UID:"1114852861896648",
+      message:{
+        payload: "ITEM_LOOKUP",
+        action: "postback"
+      }});
+
+    });
+
+  });
 
 });
