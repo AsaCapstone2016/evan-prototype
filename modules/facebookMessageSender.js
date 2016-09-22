42d944199212207e9f7f2401757d978ced0bbe04
Evan Moran <moraneva@msu.edu>
2016-09-21 20:11:07 -0400

    Added another module and changed name of test file

---
+++ modules/facebookMessageSender.js
@@ -0,0 +1,12 @@
+var facebookMessageSender = {
+  
+  //Input : {
+  //          recipientId: ID of person to send message to.
+  //          text: message of text
+  //        }
+  sendTextMessage: function(){
+      
+  }
+}
+
+module.exports = facebookMessageSender;
